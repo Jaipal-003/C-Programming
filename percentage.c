5209e006c278a9090b1b0da66c3a5e24560d70fd
Jaipal_ <jaipalsinghsisodiya46@gmail.com>
2023-09-03 22:19:04 +0530

    Initial commet

---
+++ percentage.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float m1 = 75;
+    float m2 = 84;
+    float m3 = 78;
+    float m4 = 56;
+    float m5 = 89;
+    float p =(m1 + m2 + m3 + m4 + m5)/5;
+    printf("Percentage of 5 subjects is : %f",p);
+    return 0;
+}
