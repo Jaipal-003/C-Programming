24f3d79fa228215800144aec0a491349a9765f2d
Jaipal_ <jaipalsinghsisodiya46@gmail.com>
2023-09-04 19:22:29 +0530

    some variables are changes

--- simpleInterest.c
+++ simpleInterest.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main(){
-    float principal,rate,time,si; // Four box are created
+    float principal,rate,time,simpleInterest; // Four box are created
     printf("Enter Principal :");
     scanf("%f",&principal);
     printf("Enter Rate :");
     scanf("%f",&rate);
     printf("Enter Time :");
     scanf("%f",&time);
-    si = (principal*rate*time)/100;
-    printf("Your Simple Interest is : %f",si);
+    simpleInterest = (principal*rate*time)/100;
+    printf("Your Simple Interest is : %f",simpleInterest);
     return 0;
 }
